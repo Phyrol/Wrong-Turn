c2832818f65178a34d5c9101384cb6ffe7dffd75
BrandonO <bobier1998@gmail.com>
2019-10-07 13:44:16 -0700

    SpawnPoints/BossNoise
    Put the spawn points for the items around the map and added a monster roar that goes of every couple of minutes

---
+++ Assets/Scripts/BossScream.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScream : MonoBehaviour
+{
+    AudioSource myAudio;
+    int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        print(count);
+        if (count >= 5400)
+        {
+            myAudio.Play(0);
+            count = 0;
+        }
+    }
+}
